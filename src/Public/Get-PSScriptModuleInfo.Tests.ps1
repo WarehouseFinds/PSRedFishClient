be938f70912b5fbca774588c468b45abc436fed2
Marko Stanojevic <m.stanojevic@outlook.com>
2025-12-27 14:55:38 +0100

    Switch to guid instead of name

--- src/Public/Get-PSScriptModuleInfo.Tests.ps1
+++ src/Public/Get-PSScriptModuleInfo.Tests.ps1
@@ -7,7 +7,7 @@ Describe "Get-PSScriptModuleInfo Tests" {
     It "Should return module info for existing module" {
         $moduleInfo = Get-PSScriptModuleInfo -ModulePath (Join-Path -Path $PSScriptRoot -ChildPath "../PSScriptModule.psd1")
         $moduleInfo | Should -Not -Be $null
-        $moduleInfo.Name | Should -Be "PSScriptModule"
+        $moduleInfo.GUID | Should -Be "a620849b-e411-460a-86ef-4c9e82a170ee"
     }
 
     It "Should throw an error for non-existing module" {
